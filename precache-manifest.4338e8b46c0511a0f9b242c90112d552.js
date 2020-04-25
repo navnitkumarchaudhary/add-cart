63e70c28a7ab3cb374630a0c64d561211864d865
navnit <navnit.chaudhary@misport.com>
2020-04-26 03:49:55 +0530

    Updates

---
+++ precache-manifest.4338e8b46c0511a0f9b242c90112d552.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e47bb363986f543238baa78e536dd46",
+    "url": "/add-cart/index.html"
+  },
+  {
+    "revision": "9a6ead7f8cd5190f565d",
+    "url": "/add-cart/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "1a0d65a423aec3b5cd5d",
+    "url": "/add-cart/static/css/main.aac03674.chunk.css"
+  },
+  {
+    "revision": "9a6ead7f8cd5190f565d",
+    "url": "/add-cart/static/js/2.7be72705.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/add-cart/static/js/2.7be72705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0d65a423aec3b5cd5d",
+    "url": "/add-cart/static/js/main.08d4ab98.chunk.js"
+  },
+  {
+    "revision": "9b2f83c3972cffef8fb8",
+    "url": "/add-cart/static/js/runtime-main.126ec4d2.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/add-cart/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "51c4dba69fad377ae94eb6c190eb8ff9",
+    "url": "/add-cart/static/media/star.51c4dba6.png"
+  }
+]);
